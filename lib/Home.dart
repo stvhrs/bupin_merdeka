9b44962ecfc35296aa74c511ddb3f95495e9d74f
stvhrs <steve.harris250105@gmail.com>
2024-08-19 04:24:28 +0700

    Tablet HUT RI

--- lib/Home.dart
+++ lib/Home.dart
@@ -200,7 +200,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
             onTap: _onItemTapped,
           ),
         ),
-        //  const HalamanBanner()
+          const HalamanBanner()
       ],
     );
   }
